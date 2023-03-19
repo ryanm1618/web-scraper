4719b90241f8faef3a2afa1fc19d2fb04a6ee3fb
ryanm1618 <rmiller11235@gmail.com>
2023-03-18 22:28:09 -0400

    Added some UI code, will transition to TKinter instead of using console format.

--- curses_ui.py
+++ curses_ui.py
@@ -1,6 +1,32 @@
 import curses
+import time
 
 class curses_ui:
     def __init__(self):
-        pass
+        self.prog_title = "Price Mapper Version 0.0.0"
+        self.item_str = "Item Selection"
+        self.data_str = "Data Viewer"
+        self.config_str = "Configuration"
+        self.exit_str = "Exit"
+
+    def c_main(self, stdscr):
+        curses.curs_set(0)
+        self.draw_main_menu(stdscr)
+        
+        
+    def draw_main_menu(self, stdscr):
+        max_y, max_x = stdscr.getmaxyx()
+        mid_y = max_y//2
+        mid_x = max_x//2 
+
+
+        stdscr.addstr(mid_y - 2, )
+        #text_y = max_y//2
+        #text_x = max_x//2 - len(self.test_text)//2
+        #stdscr.addstr(text_y, text_x, self.test_text)
+        #stdscr.refresh()
+        time.sleep(5)
+
+    def run_gui(self):
+        curses.wrapper(self.c_main)
     
--- ebay_search.py
+++ ebay_search.py
@@ -1,13 +1,17 @@
-from web_nav import web_nav
-from file_writer import file_writer
+#from web_nav import web_nav
+#from file_writer import file_writer
+from curses_ui import curses_ui
 
-ebay_search = web_nav()
-search_item = input("Search ebay for: ")
-write_report = file_writer(ebay_search.search_ebay(search_item), 'Ebay')
-write_report.write_to_csv()
+#ebay_search = web_nav()
+#search_item = input("Search ebay for: ")
+#write_report = file_writer(ebay_search.search_ebay(search_item), 'Ebay')
+#write_report.write_to_csv()
 
 #Going to hold off on MPB for now due to the problems it's presenting.
 #Definitely want to add this in the future
 #write_report = file_writer(ebay_search.search_mpb(search_item), 'MPB')
 #write_report.write_to_csv()
-ebay_search.close()
+#ebay_search.close()
+
+program = curses_ui()
+program.run_gui()
