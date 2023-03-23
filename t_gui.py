076a7385e10f2deeb481a737e655128ad61c4b5f
ryanm1618 <rmiller11235@gmail.com>
2023-03-22 21:17:06 -0400

    Added skeleton frames and top level organization of UI

--- t_gui.py
+++ t_gui.py
@@ -5,10 +5,29 @@ import time
 class t_gui:
     def __init__(self, config):
         self.config = config
-        self.title = config.get('Program', 'program.title')
-        self.version = config.get('Program', 'program.version')
+        self.title = self.config.get('Program', 'program.title')
+        self.version = self.config.get('Program', 'program.version')
         self.window = Tk()
         self.window.title(self.title)
+
+        self.top_frame = Frame(self.window)
+        self.top_menu_frame = Frame(self.top_frame)
+        self.notebook_frame = Frame(self.top_frame)
+
+        self.top_menu = Menu(self.top_menu_frame)
+        self.notebook = ttk.Notebook(self.notebook_frame)
+        self.home_tab_frame = Frame(self.notebook)
+
+    def init_ui(self):
+        self.notebook.add(self.home_tab_frame, text=self.config.get('UI', 'ui.hometabtitle'))
+
+
+        #Packing time...
+        self.top_menu_frame.pack()
+        self.notebook_frame.pack()
+        self.home_tab_frame.pack()
+        
+        self.notebook.pack()
 
     def t_main(self):
         self.window.mainloop()
